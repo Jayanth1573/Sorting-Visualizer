7532a0c34a5ac26d1f5d46196e867a7625572601
Jayanth <jayanthambaldhage15@gmail.com>
2024-07-25 17:09:38 +0530

    Initial Commit

---
+++ Sorting Visualizer/Sorting_VisualizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sorting_VisualizerApp.swift
+//  Sorting Visualizer
+//
+//  Created by Jayanth Ambaldhage on 25/07/24.
+//
+
+import SwiftUI
+
+@main
+struct Sorting_VisualizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
